2e0be3066c926890ea3edcc5b3cfcfbb35dc6ea8
krisdev88 <kceleban09@gmail.com>
2023-04-20 12:20:21 +0200

    Add empty Screen

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
